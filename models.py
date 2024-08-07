ce852d85cfce281cd309a67cb8bd0f750778d8c2
breadbread1984 <breadbread1984@163.com>
2024-08-07 17:08:08 +0800

    first commit

---
+++ models.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import torch
+from torch import device
+from transformers import BartTokenizer, BartModel
+
+class NERE(nn.Module):
+  def __init__(self,):
+    super(NERE, self).__init__()
+    self.tokenizer = tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
+    self.backbone = BartModel.from_pretrained('facebook/bart-base')
+  def forward(self, x):
+    inputs = self.tokenizer(x, return_tensors = 'pt')
+    outputs = self.backbone(**inputs)
+    last_hidden_states = outputs.last_hidden_state
+    return last_hidden_states
+
+if __name__ == "__main__":
+  model = NERE().to(device('cuda'))
+  hidden = model("Hello, my dog is cute")
+  print(hidden)
+

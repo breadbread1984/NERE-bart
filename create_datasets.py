8c2099c36040e47a4856b3f3025558bf8ad65900
yi.xie <yi.xie@enflame-tech.com>
2024-08-18 10:49:17 +0800

    update

--- create_datasets.py
+++ create_datasets.py
@@ -5,7 +5,12 @@ from datasets import load_dataset, Split
 def load_conll04():
   trainset = load_dataset('DFKI-SLT/conll04', split = 'train', trust_remote_code = True)
   valset = load_dataset('DFKI-SLT/conll04', split = 'validation', trust_remote_code = True)
-  return trainset, valset
+  return {
+           'trainset': trainset, 
+           'valset': valset,
+           'entity_tag_num': 4,
+           'relation_tag_num': 5
+         }
 
 def load_docred():
   trainset = load_dataset('thunlp/docred', split = 'train_annotated', trust_remote_code = True)
@@ -13,5 +18,5 @@ def load_docred():
   return trainset, valset
 
 if __name__ == "__main__":
-  trainset, valset = load_conll04()
-  print(trainset)
+  data = load_conll04()
+  print(data['trainset'])

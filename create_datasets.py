f8e92cd4829c9633bd747e67450a84f9ef6ffe37
breadbread1984 <breadbread1984@163.com>
2024-08-19 08:19:58 +0000

    update

--- create_datasets.py
+++ create_datasets.py
@@ -3,7 +3,7 @@
 from datasets import load_dataset, Split
 import numpy as np
 
-def load_conll04(tokenizer):
+def load_conll04(tokenizer, max_length = 1024):
   trainset = load_dataset('DFKI-SLT/conll04', split = 'train', trust_remote_code = True)
   valset = load_dataset('DFKI-SLT/conll04', split = 'validation', trust_remote_code = True)
   max_entity_num = np.max([len(sample['entities']) for sample in trainset])
@@ -11,6 +11,9 @@ def load_conll04(tokenizer):
   entity_types = ['Peop', 'Loc', 'Org', 'Other']
   relation_types = ['Located_In', 'Work_For', 'OrgBased_In', 'Live_In', 'Kill']
   def preprocess(sample):
+    inputs = tokenizer([sample['tokens']], is_split_into_words = True, return_tensors = 'np', padding = "max_length", max_length = max_length)
+    input_ids = inputs['input_ids'][0]
+    attention_mask = inputs['attention_mask'][0]
     tokens_per_word = np.array([len(tokenizer.tokenize(word)) for word in sample['tokens']])
     entity_starts = list()
     entity_stops = list()
@@ -32,12 +35,12 @@ def load_conll04(tokenizer):
     relation_heads = np.pad(np.stack(relation_heads, axis = 0), (0, max_relation_num - len(relation_heads)), constant_values = 0)
     relation_tails = np.pad(np.stack(relation_tails, axis = 0), (0, max_relation_num - len(relation_tails)), constant_values = 0)
     relation_tags = np.pad(np.stack(relation_tags, axis = 0), (0, max_relation_num - len(relation_tags)), constant_values = len(relation_types))
-    return {'entity_starts': entity_starts, 'entity_stops': entity_stops, 'entity_tags': entity_tags,
+    return {'input_ids': input_ids, 'attention_mask': attention_mask, 'entity_starts': entity_starts, 'entity_stops': entity_stops, 'entity_tags': entity_tags,
             'relation_heads': relation_heads, 'relation_tails': relation_tails, 'relation_tags': relation_tags}
   trainset = trainset.map(preprocess)
-  trainset.set_format(type = 'torch', columns = ['entity_starts', 'entity_stops', 'entity_tags', 'relation_heads', 'relation_tails', 'relation_tags'])
+  trainset.set_format(type = 'torch', columns = ['input_ids', 'attention_mask', 'entity_starts', 'entity_stops', 'entity_tags', 'relation_heads', 'relation_tails', 'relation_tags'])
   valset = valset.map(preprocess)
-  valset.set_format(type = 'torch', columns = ['entity_starts', 'entity_stops', 'entity_tags', 'relation_heads', 'relation_tails', 'relation_tags'])
+  valset.set_format(type = 'torch', columns = ['input_ids', 'attention_mask', 'entity_starts', 'entity_stops', 'entity_tags', 'relation_heads', 'relation_tails', 'relation_tags'])
   return trainset, valset, {'entity_types': entity_types,
                             'relation_types': relation_types,
                             'max_entity_num': max_entity_num,
@@ -55,5 +58,6 @@ if __name__ == "__main__":
   trainset, valset, meta = load_conll04(tokenizer)
   train_loader = DataLoader(trainset, batch_size = 2)
   for sample in train_loader:
+    print(sample['input_ids'].shape, sample['attention_mask'])
     print(sample['entity_starts'], type(sample['entity_starts']), sample['entity_starts'].shape)
     break

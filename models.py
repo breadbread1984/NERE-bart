b3292fd0759617ed54f0090fe8a37c6e6a4577e2
breadbread1984 <breadbread1984@163.com>
2024-08-22 01:55:59 +0000

    update

--- models.py
+++ models.py
@@ -7,23 +7,16 @@ from transformers import AutoTokenizer, BartModel
 from transformers.models.bart.modeling_bart import BartDecoder
 
 class NERE(nn.Module):
-  def __init__(self, entity_tag_num, relation_tag_num, max_entity_num = 10, max_relation_num = 10, rel_weight_mode = 'independent'):
+  def __init__(self, entity_tag_num, relation_tag_num, max_entity_num = 10, max_relation_num = 10):
     assert rel_weight_mode in {'independent', 'shared', 'fixed'}
     super(NERE, self).__init__()
+    self.entity_tag_num = entity_tag_num
+    self.relation_tag_num = relation_tag_num
     self.max_entity_num = max_entity_num
     self.max_relation_num = max_relation_num
     self.tokenizer = AutoTokenizer.from_pretrained('facebook/bart-base')
     self.encoder_and_entity_decoder = BartModel.from_pretrained('facebook/bart-base')
-    if rel_weight_mode == 'independent':
-      self.relation_decoder = BartModel.from_pretrained('facebook/bart-base').decoder
-    elif rel_weight_mode == 'shared':
-      self.relation_decoder = self.encoder_and_entity_decoder.decoder
-    elif rel_weight_mode == 'fixed':
-      self.relation_decoder = BartModel.from_pretrained('facebook/bart-base').decoder
-      for param in self.relation_decoder.parameters():
-        param.requires_grad = False
-    else:
-      raise Exception('unknown relation weight mode!')
+    self.relation_decoder = BartModel.from_pretrained('facebook/bart-base').decoder
     self.entity_embed = nn.Embedding(num_embeddings = max_entity_num, embedding_dim = self.encoder_and_entity_decoder.config.d_model)
     self.entity_start = nn.Linear(self.encoder_and_entity_decoder.config.d_model, self.encoder_and_entity_decoder.config.max_position_embeddings)
     self.entity_end = nn.Linear(self.encoder_and_entity_decoder.config.d_model, self.encoder_and_entity_decoder.config.max_position_embeddings)
@@ -38,18 +31,34 @@ class NERE(nn.Module):
     entity_embed_inputs = entity_embed_inputs.to(self.encoder_and_entity_decoder.device)
     entity_embed_inputs = self.entity_embed(entity_embed_inputs) # entity_embed_inputs.shape = (batch, max_entity_num, d_model)
     outputs = self.encoder_and_entity_decoder(input_ids = input_ids, attention_mask = attention_mask, decoder_inputs_embeds = entity_embed_inputs)
-    last_hidden_states = outputs.last_hidden_state # last_hidden_state
+    last_hidden_states = outputs.last_hidden_state # last_hidden_state.shape = (batch, max_length, hidden_dim)
     # entity start
     entity_start = self.entity_start(last_hidden_states)
     # entity end
     entity_end = self.entity_end(last_hidden_states)
     # entity tag
     entity_tag = self.entity_tag(last_hidden_states)
+    entity_start_idx = torch.argmax(entity_start, dim = -1) # entity_start_idx.shape = (batch, max_entity_num)
+    entity_end_idx = torch.argmax(entity_end, dim = -1) # entity_end_idx.shape = (batch, max_entity_num)
+    entity_tag_idx = torch.argmax(entity_tag, dim = -1) # entity_tag_idx.shape = (batch, max_entity_num)
+    entity_mask = entity_tag_idx < self.entity_tag_num # entity_mask.shape = (batch, max_entity_num)
+    valid_mask = entity_start_idx < entity_end_idx # valid_entity.shape = (batch, max_entity_num)
+    mask = torch.logical_and(entity_mask, valid_mask) # mask.shape = (batch, max_entity_num)
+    batch_entities_hidden = list()
+    batch_entities_mask = list()
+    for hidden, start, end, mask in zip(last_hidden_states, entity_start_idx, entity_end_idx, mask):
+      start = torch.masked_select(start, mask) # start.shape = (entity_num,)
+      end = torch.masked_select(end, mask) # end.shape = (entity_num,)
+      entities_hidden = torch.stack([torch.mean(hidden[s:e], dim = 0) for s, e in zip(start, end)]) # entities_hidden.shape = (entity_num, hidden_dim)
+      batch_entities_hidden.append(torch.cat([entities_hidden, torch.zeros((self.max_entity_num - entities_hidden.shape[0], entities_hidden.shape[1]))], dim = 0))
+      batch_entities_mask.append(torch.cat([torch.ones(entities_hidden.shape[0]), torch.zeros(self.max_entity_num - entities_hidden.shape[0])], dim = 0))
+    entities_hidden = torch.stack(batch_entities_hidden) # entities_hidden.shape = (batch, max_entity_num, hidden_dim)
+    entities_mask = torch.stack(batch_entities_mask) # entities_mask.shape = (batch, max_entity_num)
     # 2) relationship prediction
     relation_embed_inputs = torch.tile(torch.unsqueeze(torch.range(0, self.max_relation_num - 1, dtype = torch.int32), dim = 0), (input_ids.shape[0], 1)) # relation_embed_inputs.shape = (batch, max_relation_num)
     relation_embed_inputs = relation_embed_inputs.to(self.encoder_and_entity_decoder.device)
     relation_embed_inputs = self.relation_embed(relation_embed_inputs) # relation_embed_inputs.shape = (batch, max_relation_num, d_model)
-    outputs = self.relation_decoder(encoder_hidden_states = last_hidden_states, inputs_embeds = relation_embed_inputs)
+    outputs = self.relation_decoder(encoder_hidden_states = entities_hidden, encoder_attention_mask = entities_mask, inputs_embeds = relation_embed_inputs)
     last_hidden_states = outputs.last_hidden_state
     # relation head
     relation_head = self.relation_head(last_hidden_states)

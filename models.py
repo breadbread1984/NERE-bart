df44469c5921026414d1cec092052fec662838c4
breadbread1984 <breadbread1984@163.com>
2024-08-27 02:11:33 +0000

    update

--- models.py
+++ models.py
@@ -95,11 +95,13 @@ class HungarianMatcher(nn.Module):
       span_p = span_p[mask_p,:] # span_p.shape = (pred_target_num, 2)
       span_l = span_l[mask_l,:] # span_l.shape = (label_target_num, 2)
       # 1) span loss
-      span_p = torch.unsqueeze(span_p, dim = 0) # span_p.shape = (1, pred_target_num, 2)
-      span_l = torch.unsqueeze(span_l, dim = 0) # span_l.shape = (1, label_target_num, 2)
-      span_loss = torch.cdist(span_p, span_l, p = 1) # span_loss.shape = (1, pred_target_num, label_target_num)
+      span_p_ = torch.unsqueeze(span_p, dim = 0) # span_p.shape = (1, pred_target_num, 2)
+      span_l_ = torch.unsqueeze(span_l, dim = 0) # span_l.shape = (1, label_target_num, 2)
+      span_loss = torch.cdist(span_p_, span_l_, p = 1) # span_loss.shape = (1, pred_target_num, label_target_num)
       span_loss = torch.squeeze(span_loss, dim = 0) # span_loss.shape = (pred_target_num, label_target_num)
       # 2) iou loss
+      span_p = torch.unsqueeze(span_p, dim = 1) # span_p.shape = (pred_target_num, 1, 2)
+      span_l = torch.unsqueeze(span_l, dim = 0) # span_l.shape = (1, label_target_num, 2)
       span_p_left, _ = torch.min(span_p, dim = -1) # span_p_left.shape = (pred_target_num, 1)
       span_p_right, _ = torch.max(span_p, dim = -1) # span_p_right.shape = (pred_target_num, 1)
       span_l_left, _ = torch.min(span_l, dim = -1) # span_l_left.shape = (1, label_target_num)
